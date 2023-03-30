75618b88b8f534636ee1c152b393817fda0ff215
ChaosArk <129417853+ChaosArk@users.noreply.github.com>
2023-03-30 22:12:58 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,11 +54,11 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['16029103748'] //ur owner number
+global.ownername = "ChaosKing" //ur owner name
+global.ytname = "PornHub" //ur yt chanel name
+global.socialm = "X-Anime" //ur github or insta name
+global.location = "India, Uttar Pradesh" //ur location
 
 //bot bomdy 
 global.owner = ['916909137213']
